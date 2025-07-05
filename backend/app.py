ab796eac5c72df32c2d357d3c3a2ed738510113e
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-07-05 21:20:28 +0530

    added fix

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,20 @@
 from flask import Flask
-from flask_cors import CORS
-from models import db
-from config import Config
-from routes import main
+from flask_sqlalchemy import SQLAlchemy
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
+from models import db, Employee
+from routes import api
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
-    
-    CORS(app) 
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
     db.init_app(app)
-
-    app.register_blueprint(main)
-
     with app.app_context():
         db.create_all()
-
+    app.register_blueprint(api)
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000)
