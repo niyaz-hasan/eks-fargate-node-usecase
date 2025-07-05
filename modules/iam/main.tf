dd32281a0f4f12cc4be944b58490207ce1efba31
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-07-05 13:12:24 +0530

    added name changes

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -50,7 +50,7 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only" {
 
 ####################################### IAM Role for EKS Fargate Profile ########################################################
 resource "aws_iam_role" "fargate_profile_role" {
-  name = "${local.name}-eks-fargate-profile-role-apps"
+  name = "${var.name}-eks-fargate-profile-role-apps"
 
   assume_role_policy = jsonencode({
     Statement = [{
